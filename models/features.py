689c8481f0f31b873234d397ab49d63f26524673
Henry Fried <henry-fried@hotmail.de>
2026-07-16 13:26:34 +0200

    fix day-of-year period (356->365, leap-year aware) and capacity-join index alignment in build_features

--- models/features.py
+++ models/features.py
@@ -9,21 +9,29 @@ def time_mapping( time: int, period: int,name: str,):
 def build_features(df: pd.DataFrame, capacity_df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     hour_map = time_mapping(df["hourly"].dt.hour, 24, 'hour')
     month_map = time_mapping(df["hourly"].dt.month, 12, 'months')
-    day_of_year_map = time_mapping(df["hourly"].dt.day_of_year, 356, 'day_of_year')
-    
-    year = df["hourly"].dt.year
-    capacity_matched = capacity_df.set_index(capacity_df["time_stamp"].dt.year).loc[year].reset_index(drop=True)
+    day_of_year_period = np.where(df["hourly"].dt.is_leap_year, 366, 365)
+    day_of_year_map = time_mapping(df["hourly"].dt.day_of_year, day_of_year_period, 'day_of_year')
 
-    
+    year = df["hourly"].dt.year.rename("year")
+    capacity_matched = pd.merge(
+        year.to_frame(),
+        capacity_df.assign(year=capacity_df["time_stamp"].dt.year),
+        on="year",
+        how="left",
+        validate="many_to_one",
+    )
+    capacity_cols = ["solar_generation_capacity", "wind_generation_on_capacity", "wind_generation_off_capacity"]
+    capacity_matched[capacity_cols] = capacity_matched[capacity_cols].replace(0, np.nan)
+
     features = pd.concat([month_map, day_of_year_map, hour_map, df["avg_shortwave_radiation"],
-                df["avg_wind_speed_10m"], df["avg_cloud_cover"], df["avg_temperature_2m"], 
+                df["avg_wind_speed_10m"], df["avg_cloud_cover"], df["avg_temperature_2m"],
                 df["avg_diffuse_radiation"], df["avg_wind_speed_10m_off"]], axis=1)
 
-    targets = pd.concat([(df["solar_generation"] / capacity_matched['solar_generation_capacity']).rename("solar_generation"), 
+    targets = pd.concat([(df["solar_generation"] / capacity_matched['solar_generation_capacity']).rename("solar_generation"),
                          (df["wind_generation_on"] / capacity_matched['wind_generation_on_capacity']).rename("wind_generation_on"),
                          (df['wind_generation_off'] / capacity_matched['wind_generation_off_capacity']).rename("wind_generation_off"),
-                        ((df["solar_generation"] + df["wind_generation_on"] + df['wind_generation_off']) / 
-                        (capacity_matched['solar_generation_capacity'] + capacity_matched['wind_generation_on_capacity'] + capacity_matched['wind_generation_off_capacity'])).rename("renewable_total_mw")], axis=1)
+                        ((df["solar_generation"] + df["wind_generation_on"] + df['wind_generation_off']) /
+                        (capacity_matched['solar_generation_capacity'] + capacity_matched['wind_generation_on_capacity'] + capacity_matched['wind_generation_off_capacity'])).rename("renewable_total_capacity_factor")], axis=1)
     
     return features, targets
 
